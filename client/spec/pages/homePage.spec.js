faa64e758df927e24502c6400353ea934b3a81c8
Jacqueline Lee <jacquelinelee@Jacquelines-MacBook-Pro.local>
2016-03-15 20:08:30 +1100

    About page

--- client/spec/pages/homePage.spec.js
+++ client/spec/pages/homePage.spec.js
@@ -38,6 +38,15 @@ describe('The Home Page', function() {
       });
     });
 
+    describe('left', function() {
+      it('should navigate to about page', function() {
+        spyOn(window.App, 'navigate');
+        homePage.setButtonEvents();
+        window.App.vent.trigger('left');
+        expect(window.App.navigate).toHaveBeenCalledWith('about');
+      });
+    });
+
   });
 
   describe('rendering', function() {
